c1cbc3d508d446c1d548202de1322797c8bbc31f
Lubhana <141313752+Lubhana@users.noreply.github.com>
2026-04-03 19:28:02 +0530

    Remove dummy logic from predict function
    Removed temporary dummy logic from the predict function.

--- predict.py
+++ predict.py
@@ -1,10 +1,1 @@
-import numpy as np
 
-def predict(x):
-    # TEMPORARY dummy logic (replace with your model)
-    score = np.mean(x)
-
-    if score > 30:
-        return "⚠️ Anomaly Detected"
-    else:
-        return "✅ Normal Operation"
